e1f32894198c40c0e9a02e429e0815f9b5d74e25
happyjy <okwoyjy@gmail.com>
2022-08-21 15:18:03 +0900

    강의: 타입스크립트는 변수, 매개변수, 리턴값에 타입을 붙이는 것!

---
+++ basic/basic.js
@@ -0,0 +1,18 @@
+var a = 5;
+function add(x, y) {
+    return x + y;
+}
+//---
+// 함수로 표현하는 방법에는 아래 3가지 방법이 있으며 case1,2 방법을 많이 사용한다.
+// case1
+var add1 = function (x, y) { return x + y; };
+var add2 = function (x, y) { return x + y; };
+var add3 = function (x, y) { return x + y; };
+//---
+var arr = ["123", "456"];
+var arr2 = [123, 456];
+var arr3 = [123, 456]; // 제네릭 표현 방법
+//--- tuple
+var arr4 = [123, 456, "aaa"];
+//---
+var obj = { lat: 37.5, lon: 127.5 };
---
+++ basic/basic.ts
@@ -0,0 +1,31 @@
+const a: number = 5;
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+//---
+// 함수로 표현하는 방법에는 아래 3가지 방법이 있으며 case1,2 방법을 많이 사용한다.
+
+// case1
+const add1: (x: number, y: number) => number = (x, y) => x + y;
+
+// case2
+type Add = (x: number, y: number) => number;
+const add2: Add = (x, y) => x + y;
+
+// case3
+interface Add1 {
+  (x: number, y: number): number;
+}
+const add3: Add1 = (x, y) => x + y;
+
+//---
+const arr: string[] = ["123", "456"];
+const arr2: number[] = [123, 456];
+const arr3: Array<number> = [123, 456]; // 제네릭 표현 방법
+
+//--- tuple
+const arr4: [number, number, string] = [123, 456, "aaa"];
+
+//---
+const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
